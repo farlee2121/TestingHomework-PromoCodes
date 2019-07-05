c045a887fd42e736e26d8a23faa338b58ff9da41
Spencer Farley <spencerf2@gmail.com>
2019-07-05 11:13:24 -0500

    Add promo manager

---
+++ TestingHomework-Discounts/Managers/PromoAdminManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingHomework_Discounts.Managers
+{
+    public class PromoAdminManager
+    {
+
+        public IEnumerable<PromoCode> GetAllPromos()
+        {
+            using (PromoRepository db = new PromoRepository())
+            {
+                return db.PromoCodes.ToList();
+            }
+        }
+
+        public PromoCode SavePromo(PromoCode promo)
+        {
+            using (PromoRepository db = new PromoRepository())
+            {
+                if (promo.Id == Guid.Empty)
+                {
+                    db.PromoCodes.Add(promo);
+                }
+                else
+                {
+                    db.PromoCodes.Attach(promo);
+                    db.Entry(promo).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                }
+                db.SaveChanges();
+            }
+
+            return promo;
+        }
+    }
+}
